46c5b5416462c73b37a57e918ac6e8a991bbf72c
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-11 12:01:26 +0800

    Add setup for search feeds

---
+++ app/src/main/java/com/xrojan/lrthubkotlin/activities/SearchableActivity.kt
@@ -0,0 +1,32 @@
+package com.xrojan.lrthubkotlin.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.app.SearchManager
+import android.util.Log
+
+
+/**
+ * Created by Joshua de Guzman on 11/07/2018.
+ */
+
+class SearchableActivity : BaseActivity() {
+    private val tag = SearchableActivity::class.java.simpleName
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleIntent(intent!!)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            Log.e(tag, query)
+        }
+    }
+}
+
--- app/src/main/java/com/xrojan/lrthubkotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/xrojan/lrthubkotlin/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.xrojan.lrthubkotlin.ui.main
 
 import android.os.Bundle
+import android.view.Menu
 import com.xrojan.lrthubkotlin.R
 import com.xrojan.lrthubkotlin.activities.BaseActivity
 import com.xrojan.lrthubkotlin.ui.feed.FeedsFragment
@@ -8,6 +9,13 @@ import com.xrojan.lrthubkotlin.ui.feedback.FeedbackFragment
 import com.xrojan.lrthubkotlin.ui.settings.SettingsFragment
 import com.xrojan.lrthubkotlin.ui.traincheck.TraincheckFragment
 import kotlinx.android.synthetic.main.main_activity.*
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
+import android.util.Log
+import android.widget.SearchView
+import com.xrojan.lrthubkotlin.activities.SearchableActivity
+
 
 class MainActivity : BaseActivity() {
     private lateinit var feedsFragment: FeedsFragment
@@ -47,5 +55,22 @@ class MainActivity : BaseActivity() {
 
         // Load initial view
         bnv_dashboard.selectedItemId = R.id.nav_feeds
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu.findItem(R.id.nav_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(ComponentName(this, SearchableActivity::class.java)))
+
+        searchView.queryHint = getString(R.string.search_hint)
+        searchView.isIconified = false
+
+        return true
     }
 }
